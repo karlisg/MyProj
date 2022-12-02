9a836a05550191e9f5138a832a8ca09dd99dc926
Karla Saavedra <karla.saavedra@niceincontact.com>
2022-12-02 16:21:16 -0400

    Fix bug

--- src/MyProj/Console1/Program.cs
+++ src/MyProj/Console1/Program.cs
@@ -2,8 +2,8 @@
 using Console1;
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hola Mundo!");
+Console.WriteLine("Ciao mondo!");
 Console.WriteLine("Hello, World!");
 
 Class1 c1 = new Class1();   
