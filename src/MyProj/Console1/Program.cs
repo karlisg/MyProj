38da5849585f52cc55bd8aeea14797622c2dcadc
Karla Saavedra <karla.saavedra@niceincontact.com>
2022-12-02 16:18:22 -0400

    Add new Class

--- src/MyProj/Console1/Program.cs
+++ src/MyProj/Console1/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Console1;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+Class1 c1 = new Class1();   
+c1.sayHello();
